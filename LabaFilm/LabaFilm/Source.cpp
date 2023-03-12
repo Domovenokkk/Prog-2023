67aabb742178a6080875a6b60f1748700b3a1d53
Domovenokkk <mezuevmaksim46@gmail.com>
2023-03-12 20:41:56 +0300

    Classes

---
+++ LabaFilm/LabaFilm/Source.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class FIO {
+	string name;
+	string surname;
+	string patronymic;
+public:
+
+};
+
+class Data {
+	int day;
+	int month;
+	int year;
+public:
+
+};
+
+
+class SupportService {
+	string name;
+	string numPhone;
+	string e_mail;
+public:
+
+};
+
+class Filmoteka {
+	string name;
+	Data date_of_release;
+	FIO director;
+	string duration;
+	int sum_of_money;
+	int rating;
+public:
+
+
+};
+
+
+
+void main() {
+
+
+
+}
