ecf593081c5ebec6aa3a0f81dac6f2b92a08167a
Domovenokkk <mezuevmaksim46@gmail.com>
2023-03-28 00:58:24 +0300

    TerminalClass

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,19 +1,17 @@
-#include <math.h>
-#include <locale.h>
-#include <stdio.h>
-#include <string.h>
-
+#include <iostream>
 #include <windows.h>
 #include <conio.h>
-#include <iostream>
+#include <time.h>
+#include "sys/stat.h"
 
 using namespace std;
 
 #define KEY_EXIT 27
 #define KEY_ENTER 13
-#define ARROW_KEY 224
-#define KEY_SPACE 0
-#define KEY_BACKSPACE 8
+
+#define NONE_TITLE ""
+
+#define MENU_SIZE 6
 
 #define KEY_ARROW_RIGHT 77
 #define KEY_ARROW_LEFT 75
@@ -21,67 +19,103 @@ using namespace std;
 #define KEY_ARROW_DOWN 80
 
 class Game {
-    int cards[14];
-    int count;
-
-    Game() {
-        count = 14;
-        for (int i = 0; i < count; i += 2) {
-            cards[i] = i;
-            cards[i + 1] = cards[i];
-        }
-        for (int i = 0; i < count; i += 1) {
-            swap(cards[i], cards[rand() % count]);
-        }
-    }
+public:
+	int cards[14];
+	int count;
+	Game() {
+		count = 6;
+		for (int i = 0; i < count; i += 2) {
+			cards[i] = 2 + i;
+			cards[i + 1] = cards[i];
+		}
+		for (int i = 0; i < count; i++) {
+			swap(cards[i], cards[rand() % count]);
+		}
+	}
 };
-using namespace std;
+
+void printdeck(int* openedcards, int count) {
+	system("cls");
+	for (int i = 0; i < count; i++) {
+		if (openedcards[i] != -1)
+			cout << "[" << openedcards[i] << "]";
+		else
+			cout << "[ ]";
+	}
+}
+
 int main() {
 
-    int iKey = 67;
-    int curPos = 0;
-    int size = 6;
+	system("Cards");
 
-    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO structCursorInfo;
-    GetConsoleCursorInfo(hStdOut, &structCursorInfo);
-    structCursorInfo.bVisible = FALSE;
-    SetConsoleCursorInfo(hStdOut, &structCursorInfo);
+	srand(time(0));
 
-    for (int i = 0; i < size; i++) cout << "[   ]";
-    cout << endl;
-    for (int i = 0; i < size; i++)
-        if (i == curPos) cout << "  ^  ";
-        else cout << "     ";
-    iKey = _getch();
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-    while (iKey != KEY_ENTER) {
-        switch (iKey) {
-        case KEY_ARROW_LEFT:
-            curPos--;
-            break;
-        case KEY_ARROW_RIGHT:
-            curPos++;
-            break;
-        }
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-        if (curPos < 0) { curPos = size - 1; }
-        if (curPos > size - 1) { curPos = 0; }
+	CONSOLE_CURSOR_INFO structCursorInfo;
+	GetConsoleCursorInfo(hStdOut, &structCursorInfo);
+	structCursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo(hStdOut, &structCursorInfo);
 
-        system("cls");
+	SetConsoleTextAttribute(hStdOut, 10);
 
-        for (int i = 0; i < size; i++) cout << "[   ]";
-        cout << endl;
-        for (int i = 0; i < size; i++)
-            if (i == curPos) cout << "  ^  ";
-            else cout << "     ";
-        iKey = _getch();
+	system("cls");
 
-    }
-    for (int i = 0; i < size; i++) cout << "[   ]";
-    for (int i = 0; i < size; i++)
-        if (i == curPos) cout << "  ^  ";
-        else cout << "     ";
-    return 0;
+	int iKey = 67;
+	int menupos = 0;
+	int count = 6;
+	bool win = 0;
 
-};
+	Game game;
+
+	int opened_cards[14];
+	int opened = 0;
+	char last = -1, past = -1;
+
+	for (int i = 0; i < 14; i++) { opened_cards[i] = -1; }
+	// cards: 2 - 10 J Q K A
+	while (!win) {
+		if (opened == count)
+			break;
+		int iKey = 67;
+		while (iKey != KEY_ENTER) {
+			switch (iKey) {
+			case KEY_ARROW_LEFT:
+				menupos--;
+				break;
+			case KEY_ARROW_RIGHT:
+				menupos++;
+				break;
+			}
+
+			if (menupos < 0) { menupos = MENU_SIZE - 1; }
+			if (menupos > MENU_SIZE - 1) { menupos = 0; }
+			printdeck(opened_cards, count);
+			cout << endl << " ";
+			for (int i = 0; i < menupos; i++) {
+				cout << "   ";
+			}
+			cout << "^";
+
+			iKey = _getch();
+		}
+		opened++;
+		past = last;
+		last = menupos;
+		opened_cards[menupos] = game.cards[menupos];
+		if (opened % 2 == 0) {
+			if (opened_cards[past] != opened_cards[last]) {
+				printdeck(opened_cards, count);
+				Sleep(500);
+				opened -= 2;
+				opened_cards[past] = -1;
+				opened_cards[last] = -1;
+			}
+		}
+	}
+	system("cls");
+	cout << "You win!";
+}
