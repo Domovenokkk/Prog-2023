f264ebe9232d54ccd4bbf59fd7e71785dddffca0
Domovenokkk <mezuevmaksim46@gmail.com>
2023-11-30 15:15:57 +0300

    Added tests for vector

--- gtest/STL/StlH.h
+++ gtest/STL/StlH.h
@@ -3,14 +3,59 @@
 #include <algorithm>
 #include <stdlib.h> 
 
-float division(int a, int b) {
-	if (b == 0) { // тест
-		throw std::logic_error("Error.Division on zero in function \"div\"!");
+void FillVector(std::vector<int>& v) {
+	if (v.size() != 0) { v.clear(); }
+	for (int i = 0; i < 20; i++) {
+		v.push_back(0);
 	}
-	else if (a == 0 && b < 0) { // тест
-		return 0;
+};
+
+void RandVector(std::vector<int>& v) {
+	v.clear();
+	for (int i = 0; i < v.size(); i++) {
+		int randN = (rand() % 200 + 1 - 100);
+		v.push_back(randN);
+		std::cout << v[i];
 	}
-	else { // тест
-		return a / b;
+}
+
+int MaxElem(std::vector<int> v) {
+	int max = *max_element(v.begin(), v.end());
+	return max;
+}
+
+int MinElem(std::vector<int> v) {
+	int min = *min_element(v.begin(), v.end());
+	return min;
+}
+
+void AddFromArray(std::vector<int>& v, int array[], int size) {
+	for (int i = 0; i < size; i++) {
+		v.push_back(array[i]);
 	}
+	sort(v.begin(), v.end());
+}
+
+void ReplacementWithZero(std::vector<int>& v, int n) {
+	std::replace_if(v.begin(), v.end(), [n](int& i) {
+		return i < n;
+		}, 0);
+}
+
+std::vector<int> NewVector(std::vector<int> v, int n) {
+	std::vector<int> res;
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] > n) {
+			res.push_back(v[i]);
+		}
+	}
+	return res;
+}
+
+void RandomShuffle(std::vector<int> v) {
+	std::random_shuffle(v.begin(), v.end());
+}
+
+void erasevector(std::vector<int>& v) {
+	v.clear();
 }
--- gtest/tests/tests_STL.cpp
+++ gtest/tests/tests_STL.cpp
@@ -1,17 +1,75 @@
 #include "../gtest/gtest.h"
 #include "../STL/StlH.h"
 
-TEST(test_division, throw_when_try_div_by_zero_test) {
+
+TEST(test_fillvector, check_size) {
 	// Arrange
-	int x = 6, y = 0;
-	// Act & Assert
-	ASSERT_ANY_THROW(division(x, y));
-}
-TEST(test_division, positive_zero_after_div_zero_on_negative_value_test) {
-	// Arrange
-	int x = 0, y = -5;
+	std::vector<int> v;
 	// Act
-	float res = division(x, y);
+	FillVector(v);
+	int res = v.size();
 	// Assert
+	EXPECT_EQ(res, 20);
+}
+
+TEST(test_fillvector, check_fill) {
+	std::vector<int> v;
+
+	FillVector(v);
+	int res = 0;
+	for (int i = 0; i < 20; i++) {
+		res += v[i];
+	}
 	EXPECT_EQ(res, 0);
+}
+
+///
+
+
+TEST(test_minmax_out, checkmin) {
+	std::vector<int> v = { 1, 2, 3, 4, 5 };
+
+	EXPECT_EQ(MinElem(v), 1);
+}
+
+TEST(test_minmax_out, checkmax) {
+	std::vector<int> v = { 1, 2, 3, 4, 5 };
+
+	EXPECT_EQ(MaxElem(v), 5);
+}
+
+///
+
+TEST(test_arrayaddtovec, check_size) {
+	std::vector<int> v = { 6, 2 ,1 ,5 };
+	int array[4] = { 5,2,6,9 };
+
+	AddFromArray(v, array, 4);
+
+	EXPECT_EQ(v.size(), 8);
+}
+
+
+TEST(test_cmptozero, check_vec) {
+	std::vector<int> v = { 1, 2, 3, 4, 5 };
+
+	ReplacementWithZero(v, 3);
+
+	EXPECT_EQ(v[3] + v[4], 9);
+
+}
+
+///
+
+TEST(createvectorbiggerthancmp__, check_array) {
+	std::vector<int> vec = { 1, 2, 3, 4, 5 };
+	std::vector<int> veccmp = NewVector(vec, 3);
+
+	EXPECT_EQ(veccmp[3] == 4 && veccmp[4] == 5, 1);
+}
+
+TEST(erasevector_, check_erased) {
+	std::vector<int> vec = { 1,2,3,4,5,6,7 };
+	erasevector(vec);
+	EXPECT_EQ(vec.size(), 0);
 }
